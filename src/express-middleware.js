e48ae6638362be8719ec758f68685a11bb16c616
Ziming Yuan <zimingyuan2017@gmail.com>
2024-07-03 07:37:30 -0400

    converted promise to async/await

--- src/express-middleware.js
+++ src/express-middleware.js
@@ -12,27 +12,8 @@ const HEADERS = [
   'Content-Type',
 ];
 
-function fetchListingItems(query, apiPath, authToken) {
-  return new Promise((resolve, reject) => {
-    const request = superagent
-      .post(`${apiPath}/@querystring-search`)
-      .send(query)
-      .accept('json');
-
-    if (authToken) {
-      request.set('Authorization', `Bearer ${authToken}`);
-    }
-    request.end((err, resp) => {
-      if (resp && resp.body) {
-        return resolve(resp.body.items);
-      }
-      return reject(err);
-    });
-  });
-}
-
-function fetchListingContent(apiPath, APISUFIX, req, settings) {
-  return new Promise((resolve, reject) => {
+async function fetchListingContent(apiPath, APISUFIX, req, settings) {
+  try {
     const request = superagent
       .get(
         `${apiPath}${__DEVELOPMENT__ ? '' : APISUFIX}${req.path.replace(
@@ -41,41 +22,61 @@ function fetchListingContent(apiPath, APISUFIX, req, settings) {
         )}`,
       )
       .accept('json');
+
     const authToken = req.universalCookies.get('auth_token');
     if (authToken) {
       request.set('Authorization', `Bearer ${authToken}`);
     }
 
-    request.end((err, resp) => {
-      if (resp && resp.body) {
-        const json = JSON.parse(JSON.stringify(resp.body));
-        const listingBlock = findBlocks(json.blocks, 'listing');
-        let queryData = json.blocks[listingBlock]?.querystring;
-        if (!queryData) {
-          return reject(new Error('No query data found in listing block'));
-        }
-        if (queryData?.sort_order !== null) {
-          if (typeof queryData.sort_order === 'boolean') {
-            queryData.sort_order = queryData.sort_order
-              ? 'descending'
-              : 'ascending';
-          }
-        }
+    const response = await request;
+    const json = JSON.parse(JSON.stringify(response.body));
+    const listingBlock = findBlocks(json.blocks, 'listing');
+    let queryData = json.blocks[listingBlock]?.querystring;
 
-        let query = {
-          ...queryData,
-          ...(!queryData.b_size && {
-            b_size: settings.defaultPageSize,
-          }),
-          query: queryData?.query,
-          metadata_fields: '_all',
-          b_start: 0,
-        };
-        resolve(query);
+    if (!queryData) {
+      throw new Error('No query data found in listing block');
+    }
+
+    if (queryData?.sort_order !== null) {
+      if (typeof queryData.sort_order === 'boolean') {
+        queryData.sort_order = queryData.sort_order
+          ? 'descending'
+          : 'ascending';
       }
-      reject(err);
-    });
-  });
+    }
+
+    let query = {
+      ...queryData,
+      ...(!queryData.b_size && {
+        b_size: settings.defaultPageSize,
+      }),
+      query: queryData?.query,
+      metadata_fields: '_all',
+      b_start: 0,
+    };
+
+    return query;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function fetchListingItems(query, apiPath, authToken) {
+  try {
+    const request = superagent
+      .post(`${apiPath}/@querystring-search`)
+      .send(query)
+      .accept('json');
+
+    if (authToken) {
+      request.set('Authorization', `Bearer ${authToken}`);
+    }
+
+    const response = await request;
+    return response.body.items;
+  } catch (err) {
+    throw err;
+  }
 }
 
 function make_rssMiddleware(config) {
@@ -90,61 +91,61 @@ function make_rssMiddleware(config) {
     apiPath = settings.apiPath;
   }
 
-  function rssMiddleware(req, res, next) {
-    fetchListingContent(apiPath, APISUFIX, req, settings)
-      .then((query) => {
-        fetchListingItems(
-          query,
-          apiPath,
-          req.universalCookies.get('auth_token'),
-        )
-          .then((items) => {
-            const feed = new Feed({
-              title: 'RSS Feed',
-              description: 'Plone Site RSS Feed',
-              id: settings.publicURL,
-              generator: 'EEA Website',
-              link: settings.publicURL,
-              feedLinks: {
-                rss: `${settings.publicURL}${req.path}`,
-              },
-            });
+  async function rssMiddleware(req, res, next) {
+    try {
+      const query = await fetchListingContent(apiPath, APISUFIX, req, settings);
+      const items = await fetchListingItems(
+        query,
+        apiPath,
+        req.universalCookies.get('auth_token'),
+      );
 
-            items.forEach((item) => {
-              feed.addItem({
-                id: toPublicURL(item['@id']),
-                title: item.title,
-                description: item.description,
-                date: item.last_modified,
-              });
-            });
+      const feed = new Feed({
+        title: 'RSS Feed',
+        description: 'Plone Site RSS Feed',
+        id: settings.publicURL,
+        generator: 'EEA Website',
+        link: settings.publicURL,
+        feedLinks: {
+          rss: `${settings.publicURL}${req.path}`,
+        },
+      });
 
-            const result = feed.rss2();
-            res.setHeader('content-type', 'application/rss+xml');
-            res.send(result);
-          })
-          .catch(next);
-      })
-      .catch(next);
+      items.forEach((item) => {
+        feed.addItem({
+          id: toPublicURL(item['@id']),
+          title: item.title,
+          description: item.description,
+          date: item.last_modified,
+        });
+      });
+
+      const result = feed.rss2();
+      res.setHeader('content-type', 'application/rss+xml');
+      res.send(result);
+    } catch (err) {
+      next(err);
+    }
   }
 
   return rssMiddleware;
 }
 
-function viewMiddleware(req, res, next) {
-  getAPIResourceWithAuth(req)
-    .then((resource) => {
-      // Just forward the headers that we need
-      HEADERS.forEach((header) => {
-        if (resource.get(header)) {
-          res.set(header, resource.get(header));
-        }
-      });
-      //check if we have listing items here
-      res.status(resource.statusCode);
-      res.send(resource.body);
-    })
-    .catch(next);
+async function viewMiddleware(req, res, next) {
+  try {
+    const resource = await getAPIResourceWithAuth(req);
+    // Just forward the headers that we need
+    HEADERS.forEach((header) => {
+      if (resource.get(header)) {
+        res.set(header, resource.get(header));
+      }
+    });
+    //check if we have listing items here
+    res.status(resource.statusCode);
+    res.send(resource.body);
+  } catch (err) {
+    next(err);
+  }
 }
 
 export default function makeMiddlewares(config) {
