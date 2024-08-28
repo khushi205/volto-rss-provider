6bdf08efbedda8c26f542eb4660f2f277bc0fee5
Ziming Yuan <zimingyuan2017@gmail.com>
2024-08-27 23:36:58 -0400

    set required and disableNewBlocks in initialBlocks

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const applyConfig = (config) => {
 
   config.blocks.initialBlocks = {
     ...config.blocks.initialBlocks,
-    rss_feed: ['title', 'listing'],
+    rss_feed: [
+      { '@type': 'title', fixed: true, required: true, disableNewBlocks: true },
+      {
+        '@type': 'listing',
+        fixed: true,
+        required: true,
+        disableNewBlocks: true,
+      },
+    ],
   };
 
   config.views.contentTypesViews.rss_feed = RSSFeedView;
